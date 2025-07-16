d93879fb1ddc2b4d0ef4bac071cff9a96ebb5644
yoshi <yoshimasa@w-japan.net>
2025-07-16 13:41:05 -0700

    task has done

--- game.js
+++ game.js
@@ -2,6 +2,9 @@ alert("HEllo this is Katsuki from javascript");
 
 alert("HEllo this is Katsuki from javascript222");
 
-function findAvg(num1, num2, num3){
-    return (num1+num2+num3)/3;
+function findAvg(num1, num2, num3) {
+  return (num1 + num2 + num3) / 3;
+}
+function sum(num1, num2) {
+  return num1 + num2;
 }
